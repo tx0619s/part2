19fe3d495f071e380dced1fd27912b3730cb15a0
Xiaocheng-Zhang <59710148+Xiaocheng-Zhang@users.noreply.github.com>
2020-01-30 16:40:27 -0500

    added two getter methods

--- map.h
+++ map.h
@@ -8,17 +8,23 @@
 // Later a list of Hashmap_pair will be stored into Hashmap data structure.
 // It inherit Object class.
 class Hashmap_pair : public Object {
-    public:
-        Object* key_;
-        Object* val_;
-        Hashmap_pair(Object *key, Object *val) : Object() {
-            key_ = key;
-            val_ = val;
-        }
-        ~Hashmap_pair() {
-            delete key_;
-            delete val_;
-        }
+public:
+  Object *key_;
+  Object *val_;
+  Hashmap_pair(Object *key, Object *val) : Object() {
+    key_ = key;
+    val_ = val;
+  }
+  ~Hashmap_pair() {
+    delete key_;
+    delete val_;
+  }
+
+  // getter method that help to get value from Hashmap_pair.
+  Object* getVal() { return val_; }
+
+  // getter method that help to get key from Hashmap_pair.
+  Object* getKey() { return key_; }
 };
 
 // Hashmap class stores a list of hashmap_pairs, which contains equal number
@@ -26,43 +32,41 @@ class Hashmap_pair : public Object {
 // It has size and capcity, which size is the number of key-value pairs,
 // and capcity is the physical size of hashmap.
 class Hashmap : public Object {
-    public:
-        Hashmap_pair **data;
-        size_t size_;
-        size_t capacity_; 
-        //constructor
-        //capcity will be initilized as 4, size is 0 by default.
-        Hashmap() {
-            data = new Hashmap_pair*[4];
-            size_ = 0;
-            capacity_ = 4;
-        }
+public:
+  Hashmap_pair **data;
+  size_t size_;
+  size_t capacity_;
+  // constructor
+  // capcity will be initilized as 4, size is 0 by default.
+  Hashmap() {
+    data = new Hashmap_pair *[4];
+    size_ = 0;
+    capacity_ = 4;
+  }
 
-        // destructor
-        ~Hashmap() {
-            delete [] data;
-        }
+  // destructor
+  ~Hashmap() { delete[] data; }
 
-        // Double the capacity of hashmap when needed
-        void expand();
+  // Double the capacity of hashmap when needed
+  void expand();
 
-        // Returns the value to which the specified key is mapped, 
-        // or null if this map contains no mapping for the key.
-        Object* get(Object *key);
+  // Returns the value to which the specified key is mapped,
+  // or null if this map contains no mapping for the key.
+  Object *get(Object *key);
 
-        // Associates the specified value with the specified key in this map.
-        void put(Object *key, Object *val);
+  // Associates the specified value with the specified key in this map.
+  void put(Object *key, Object *val);
 
-        // Removes the mapping for the specified key from this map if present.
-        void remove(Object *key);
+  // Removes the mapping for the specified key from this map if present.
+  void remove(Object *key);
 
-        // Returns the number of key-value mappings in this map.
-        size_t size();
+  // Returns the number of key-value mappings in this map.
+  size_t size();
 
-        // Returns a list view of the keys contained in this map.
-        Object** key_array();
+  // Returns a list view of the keys contained in this map.
+  Object **key_array();
 
-        // Check if two Hashmaps are equal.
-        // the input hashmap is an object.
-        bool equals(Object *map);
+  // Check if two Hashmaps are equal.
+  // the input hashmap is an object.
+  bool equals(Object *map);
 };
